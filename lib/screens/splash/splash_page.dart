9944c9fa10a98eeefd4ac90005e91c1ea89a3669
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-05 17:29:09 +0700

    load resources via flutter_gen.

--- lib/screens/splash/splash_page.dart
+++ lib/screens/splash/splash_page.dart
@@ -1,3 +1,4 @@
+import 'package:d3f_dependency_manager/dependency_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:get/get.dart';
@@ -17,7 +18,7 @@ class SplashPage extends HookWidget {
         return Scaffold(
           backgroundColor: Colors.white,
           body: Center(
-            child: Lottie.asset('packages/d3f_shared/assets/lotties/splash_animation.json',
+            child: Lottie.asset('packages/d3f_shared/${MyAssets.lotties.splashAnimation.path}',
                 controller: _animationController, onLoaded: (composition) {
               _animationController
                 ..duration = composition.duration
