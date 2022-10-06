ab5a0807f34e31b59bec656c8afd4723e3ab85cf
DanhDue ExOICTIF <danhdue@gmail.com>
2022-10-06 15:51:14 +0700

    integrate the binding method for all modules.

--- lib/app_global_binding.dart
+++ lib/app_global_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
 import 'package:dio/dio.dart';
+import 'package:d3f_login/app_global_binding.dart';
 
 class AppGlobalBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(() => Dio());
+    LoginGlobalBinding().dependencies();
+    Get.put(() => Dio(), permanent: true);
   }
 }
