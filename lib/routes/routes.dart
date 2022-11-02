4cce118ee994a68506f4701f3984e66ea2506fd0
DanhDue ExOICTIF <danhdue@gmail.com>
2022-11-02 14:14:29 +0700

    ignore searching, changes watching, and viewing the unnecessary files.

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: depend_on_referenced_packages
 
-import 'package:d3f_login/routes/routes.dart';
+import 'package:d3f_login/app/routes/routes.dart';
 import 'package:d3f_modular/screens/splash/splash_page.dart';
 import 'package:get/get.dart';
 
