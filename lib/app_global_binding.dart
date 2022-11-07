c6f9766aad535fc653b358d927c2c6b0820316f0
DanhDue ExOICTIF <danhdue@gmail.com>
2022-11-07 11:39:39 +0700

    refactor the DI module.

--- lib/app_global_binding.dart
+++ lib/app_global_binding.dart
@@ -1,5 +1,7 @@
-// ignore_for_file: depend_on_referenced_packages
+// ignore_for_file: depend_on_referenced_packages, unused_import
 
+import 'package:d3f_core/core.dart';
+import 'package:d3f_inapp_purchase/app/app_global_binding.dart';
 import 'package:d3f_login/app/app_global_binding.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
@@ -7,7 +9,8 @@ import 'package:get/get.dart';
 class AppGlobalBinding extends Bindings {
   @override
   void dependencies() {
-    LoginGlobalBinding().dependencies();
-    Get.put(() => Dio(), permanent: true);
+    D3FCoreGlobalBindings().dependencies();
+    LoginGlobalBindings().dependencies();
+    InAppPurchaseGlobalBinding().dependencies();
   }
 }
